48f71ed11bf308acf78b2c17dca9a87e2ac24b7f
vomicz <t.vomacka@seznam.cz>
2025-06-11 11:05:32 +0200

    Container specs improved

--- Assets/Scripts/Interaction/Container.cs
+++ Assets/Scripts/Interaction/Container.cs
@@ -5,8 +5,8 @@ using UnityEngine;
  * 
  * 1 - can be opened and displays items inside, these items can be picked or player items can be placed inside, remembers its content
  * 2 - should check if the player is in interactible range and visually notify by displaying text, changing color, ...
- * 3 - should be open/closed
- * 4 - if closed and player can open (key, lockpick, ...) provide appropriate interactivity
+ * 3 - should be open/locked
+ * 4 - if locked and player can open (key, lockpick, ...) provide appropriate interactivity
  * 
  */
 public class Container : MonoBehaviour
